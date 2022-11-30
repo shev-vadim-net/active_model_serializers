c4d12ea6ed2cdc8273ccecea78d03b825fa17d86
Vadim Shevchenko <shev.vadim.net@gmail.com>
2022-11-30 13:27:21 +0700

    Make a serializer class lookup work the same incorrect way as it was before Ruby 2.7 + Bump to 0.9.9

--- lib/active_model/serializable/utils.rb
+++ lib/active_model/serializable/utils.rb
@@ -3,10 +3,19 @@ module ActiveModel
     module Utils
       extend self
 
+      # It seems like the Ruby 2.7 changes the behaviour of the `Object.const_get method`.
+      # In the previous versions the `Object.const_get('Projects::EBookSerializer')` returns `EBookSerializer`,
+      # but now it raises `NameError`.
+      # We totally understand the Ruby's 2.7 behaviour is now fixed and the previous versions resolved
+      # a constant name in an incorrect way, but we don't want to invest additional time to fix all these
+      # issues across the project. Especially when we have plans to replace this serializer with a newer one.
+      #
+      # So the workaround is to get a constant via `Object.const_get('Projects').const_get('EBookSerializer')`
+      # which seems to work the same as in the previous Ruby version - means incorrectly.
+      # See some info here https://gitlab.com/gitlab-org/gitlab/-/issues/27678
       def _const_get(const)
         begin
-          method = RUBY_VERSION >= '2.0' ? :const_get : :qualified_const_get
-          Object.send method, const
+          const.split('::').inject(Object) { |obj, const_name| obj.send(:const_get, const_name) }
         rescue NameError
           const.safe_constantize
         end
